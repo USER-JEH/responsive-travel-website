fc1549372800cb873f9fd869cc56cfd8990d8100
USER-JEH <han880330@yahoo.co.jp>
2021-06-29 16:59:03 +0900

    about&discover&experience&video

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,19 +36,48 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*==================== SWIPER DISCOVER ====================*/
-
+var swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween:32,
+    coverflowEffect: {
+      rotate: 0,
+    },
+  });
 /*==================== VIDEO ====================*/
+const videoFile = document.getElementById('video-file'),
+      videoButton = document.getElementById('video-button'),
+      videoIcon = document.getElementById('video-icon')
 
-        // Play video
-      
-        // Pause video
-     
-        // We change the icon
-   
-    // Video ends, icon change
-   
+function playPause() {
+  if (videoFile.paused) {
+    // Play video
+    videoFile.play()
+
+     // We change the icon
+    videoIcon.classList.add('ri-pause-line')
+    videoIcon.classList.remove('ri-play-line')
+  } else {
+    // Pause video
+    videoFile.pause()
+     // We change the icon
+     videoIcon.classList.remove('ri-pause-line')
+    videoIcon.classList.add('ri-play-line')
+  }
+}
+       
+videoButton.addEventListener('click',playPause)
+       
+function finalVideo() {
+  // Video ends, icon change
+    videoIcon.classList.remove('ri-pause-line')
+    videoIcon.classList.add('ri-play-line')
+}
 // ended, when the video ends
-
+videoFile.addEventListener('ended', finalVideo)
 
 
 /*==================== SHOW SCROLL UP ====================*/ 
